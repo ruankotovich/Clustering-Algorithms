b18d4b4c98d6b731d71d33fe014610d879181826
Kotovich <rggb@icomp.ufam.edu.br>
2018-11-17 19:50:07 -0400

    [INS] testcases

---
+++ algs/bases/pieces/parser.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+(() => {
+    let testCollection = [];
+
+    for (let file of fs.readdirSync('./').filter((k) => path.extname(k).toLowerCase() === '.piece')) {
+        let originalObject = JSON.parse(fs.readFileSync(file, 'utf-8'));
+        testCollection.push({
+            bussiness: originalObject.business,
+            input: originalObject.text,
+            expected: originalObject.entities.map(
+                el => el.label
+            )
+        });
+    }
+    fs.writeFileSync('../testcollection.json', JSON.stringify(testCollection, null, 2));
+})();
